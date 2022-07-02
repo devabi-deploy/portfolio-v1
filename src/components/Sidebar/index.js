782d31e24e9f63731c544b130d5fd411a9b9671c
devabi-deploy <devabi661528@gmail.com>
2022-07-02 12:07:59 +0700

    Sidebar part 1 img logo and logo subtitle

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,16 @@
-import styles from './index.scss';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import LogoS from './../../assets/img/logo.png';
+import LogoSubtitle from './../../assets/img/logo-subtitle.png';
 
 const Sidebar = () => {
     return (
-        <>
-            This is a sidebar
-        </>
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={ LogoS } alg="Logo" />
+                <img src= { LogoSubtitle } alt="Ma-name" className="sub-logo" />
+            </Link>
+        </div>
     )
 }
 
