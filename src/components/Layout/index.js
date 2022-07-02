72c0f4c5fd9a5a48090b5bf276df148b05588ff3
devabi-deploy <devabi661528@gmail.com>
2022-07-02 11:27:03 +0700

    Inisiasi sudah selesai

---
+++ src/components/Layout/index.js
@@ -0,0 +1,12 @@
+import styles from './index.scss';
+import Sidebar from './../Sidebar/index';
+
+const Layout = () => {
+    return (
+        <>
+            <Sidebar />
+        </>
+    )
+}
+
+export default Layout;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,11 @@
+import styles from './index.scss';
+
+const Sidebar = () => {
+    return (
+        <>
+            This is a sidebar
+        </>
+    )
+}
+
+export default Sidebar;
