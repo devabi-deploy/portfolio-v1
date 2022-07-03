31a7938475642d96e2fcf2fad0dcf89f5a4899d1
devabi-deploy <devabi661528@gmail.com>
2022-07-03 20:21:38 +0700

    Progress home page is a 70%

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,13 +6,12 @@ const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm
-                <img src={LogoTitle} alt="Ma-Logo" />
+                <h1>Hii, <br/> I'm
                 Bimanyu Nugroho
+                <img src={LogoTitle} alt="Ma-Logo" />
                 <br/>
-                Web Developer
                 </h1>
-                <h2>Fullstack Developer | Content creator IT</h2>
+                <h2>Web Developer / PHP Intermidate</h2>
                 <Link to="/contact" className="flat-button">Touch Me</Link>
             </div>
         </div>
